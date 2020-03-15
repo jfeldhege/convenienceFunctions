b43c9a1f10508402229f158cbe423a4331a42fcd
jfeldhege <jfeldhege+git@gmail.com>
2020-03-15 11:28:26 +0100

    Add function to create new chapter in RStudio

--- R/functions.R
+++ R/functions.R
@@ -8,12 +8,11 @@
 #' @export
 #'
 #' @examples
-#'
 #' iris$Sepal.Length[4] <- NA
-#' length_na(iris$Sepal.Length)
-#' length_na(iris$Sepal.Length, missing = FALSE)
-#'
-length_na <- function(vector,
+#' length_NA(iris$Sepal.Length)
+#' length_NA(iris$Sepal.Length, missing = FALSE)
+
+length_NA <- function(vector,
                       missing = TRUE){
 
   if(missing){
@@ -25,4 +24,44 @@ length_na <- function(vector,
   len <- length(which(inp))
 
   return(len)
+}
+
+
+#' Count values that satisfy a condition
+#'
+#' @param condition The condition for which values are to be counted. Must be
+#' type logical
+#'
+#' @return An integer with the count of values that satisfy the condition.
+#' @export
+
+length_which <- function(condition) {
+
+  assertthat::assert_that(is.logical(condition), msg = "Value is not a logical")
+
+  return(length(which(condition)))
+}
+
+
+#' Sets new chapter in R script in RStudio
+#'
+#' @return Creates a vertical line in R script
+#' @export
+#' @references https://www.statworx.com/de/blog/defining-your-own-shortcut-in-rstudio/
+
+
+set_new_chapter <- function(){
+  # set limit to which position dashes should be included
+  nchars <- 81
+
+  # grab current document information
+  context <- rstudioapi::getActiveDocumentContext()
+  # extract horizontal courser position in document
+  context_col <- context$selection[[1]]$range$end["column"]
+
+  # if a line has less than 81 characters, insert hyphens at the current line
+  # up to 80 characters
+  if (nchars > context_col) {
+    rstudioapi::insertText(strrep("-", nchars - context_col))
+  }
 }
